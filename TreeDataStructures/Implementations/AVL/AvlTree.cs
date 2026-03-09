8aff39a4e6d51c15f657ec81b28fa56d904d46a9
Awak3r <bogdanovmihail129@gmail.com>
2026-03-09 23:23:23 +0300

    Implement AVL balancing and Splay tree operations

--- TreeDataStructures/Implementations/AVL/AvlTree.cs
+++ TreeDataStructures/Implementations/AVL/AvlTree.cs
@@ -7,11 +7,57 @@ public class AvlTree<TKey, TValue> : BinarySearchTreeBase<TKey, TValue, AvlNode<
 {
     protected override AvlNode<TKey, TValue> CreateNode(TKey key, TValue value)
         => new(key, value);
+
+    private static int GetHeight(AvlNode<TKey, TValue>? node) => node?.Height ?? 0;
+
+    private static void UpdateHeight(AvlNode<TKey, TValue> node)
+    {
+        int leftHeight = GetHeight(node.Left);
+        int rightHeight = GetHeight(node.Right);
+        int maxHeight = leftHeight > rightHeight ? leftHeight : rightHeight;
+        node.Height = 1 + maxHeight;
+    }
+
+    private static int GetBalance(AvlNode<TKey, TValue> node)
+    {
+        return GetHeight(node.Left) - GetHeight(node.Right);
+    }
+
+    private void Zabalansit(AvlNode<TKey, TValue>? node)
+    {
+        while (node != null)
+        {
+            UpdateHeight(node);
+            int balance = GetBalance(node);
+            if (balance > 1)
+            {
+                if (node.Left != null && GetBalance(node.Left) < 0)
+                {
+                    RotateLeft(node.Left);
+                    UpdateHeight(node.Left);
+                }
+                RotateRight(node);
+                UpdateHeight(node);
+                if (node.Parent != null) { UpdateHeight(node.Parent); }
+            }
+            else if (balance < -1)
+            {
+                if (node.Right != null && GetBalance(node.Right) > 0)
+                {
+                    RotateRight(node.Right);
+                    UpdateHeight(node.Right);
+                }
+                RotateLeft(node);
+                UpdateHeight(node);
+                if (node.Parent != null) { UpdateHeight(node.Parent); }
+            }
+            node = node.Parent;
+        }
+    }
     
     protected override void OnNodeAdded(AvlNode<TKey, TValue> newNode)
     {
-        throw new NotImplementedException();
+        Zabalansit(newNode);
     }
 
-    
 }
--- TreeDataStructures/Implementations/Splay/SplayTree.cs
+++ TreeDataStructures/Implementations/Splay/SplayTree.cs
@@ -8,20 +8,90 @@ public class SplayTree<TKey, TValue> : BinarySearchTree<TKey, TValue>
 {
     protected override BstNode<TKey, TValue> CreateNode(TKey key, TValue value)
         => new(key, value);
+
+    private void Splay(BstNode<TKey, TValue> node)
+    {
+        while (node.Parent != null)
+        {
+            BstNode<TKey, TValue> parent = node.Parent;
+            BstNode<TKey, TValue>? grand = parent.Parent;
+
+            if (grand == null)
+            {
+                if (node.IsLeftChild) { RotateRight(parent); }
+                else { RotateLeft(parent); }
+            }
+            else if ((node.IsLeftChild && parent.IsLeftChild) || (node.IsRightChild && parent.IsRightChild))
+            {
+                if (node.IsLeftChild)
+                {
+                    RotateRight(grand);
+                    RotateRight(parent);
+                }
+                else
+                {
+                    RotateLeft(grand);
+                    RotateLeft(parent);
+                }
+            }
+            else
+            {
+                if (node.IsLeftChild)
+                {
+                    RotateRight(parent);
+                    RotateLeft(grand);
+                }
+                else
+                {
+                    RotateLeft(parent);
+                    RotateRight(grand);
+                }
+            }
+        }
+    }
     
     protected override void OnNodeAdded(BstNode<TKey, TValue> newNode)
     {
-        throw new NotImplementedException();
+        Splay(newNode);
     }
     
     protected override void OnNodeRemoved(BstNode<TKey, TValue>? parent, BstNode<TKey, TValue>? child)
     {
-        throw new NotImplementedException();
+        if (parent != null)
+        {
+            Splay(parent);
+        }
+        else if (child != null)
+        {
+            Splay(child);
+        }
+    }
+
+    public override bool ContainsKey(TKey key)
+    {
+        return TryGetValue(key, out _);
     }
     
     public override bool TryGetValue(TKey key, [MaybeNullWhen(false)] out TValue value)
     {
-        throw new NotImplementedException();
+        BstNode<TKey, TValue>? current = Root;
+        BstNode<TKey, TValue>? last = null;
+        while (current != null)
+        {
+            last = current;
+            int cmp = Comparer.Compare(key, current.Key);
+            if (cmp == 0)
+            {
+                value = current.Value;
+                Splay(current);
+                return true;
+            }
+            current = cmp < 0 ? current.Left : current.Right;
+        }
+
+        if (last != null) { Splay(last); }
+        value = default;
+        return false;
     }
     
 }
